f9674e00c68f939807ab086108324ca9bf5d200a
Dani3969 <106531996+Dani3969@users.noreply.github.com>
2024-11-12 09:29:35 -0300

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProductosComponent } from './components/productos/productos.component';
-
-export const routes: Routes = [
-  { path: 'productos', component: ProductosComponent },
-];
